15fb40749d653a64b70c5124301781625b90572c
Ann <a.m.hnatyshyn@gmail.com>
2024-07-18 17:45:09 -0300

    edit orders seed file

--- db/seeds/02_orders.sql
+++ db/seeds/02_orders.sql
@@ -1,17 +1,17 @@
 INSERT INTO orders (id, created_at, total_amount, pick_up_time, message,
  order_status, confirmation, customer_id) VALUES(
-  1, 14:30:00, 17.00, , "Your order has been placed",
+  1, 14:30:00, 17.00, 15:00:00, "Your order has been placed",
   "Order Received", true, 1)
 );
 
 INSERT INTO orders (id, created_at, total_amount, pick_up_time, message,
  order_status, confirmation, customer_id) VALUES(
-  2, 12:30:00, 17.00, "Your order has been placed",
+  2, 12:30:00, 17.00, 13:00:00, "Your order has been placed",
   "Order Received", true, 2)
 );
 
 INSERT INTO orders (id, created_at, total_amount, pick_up_time, message,
  order_status, confirmation, customer_id) VALUES(
-  3, 3, 12.00, 18:00:00, "Your order has been placed",
-  "Order Received", true
+  3, 13:00:00, 12.00, 13:30:00, "Your order has been placed",
+  "Order Received", true, 3
 );
