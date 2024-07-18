fef256e0f1bd12b1f6db44967ae5876c43e9d1a8
Ann <a.m.hnatyshyn@gmail.com>
2024-07-18 17:23:22 -0300

    add menu_items to seeds

---
+++ db/seeds/04_menu_items.sql
@@ -0,0 +1,34 @@
+INSERT INTO menu_items (
+  1, "Mac Attack Burger", "A double patty with crispy edges and a juicy center, topped with melted cheddar, lettuce, tomato, pickles, and house-made sauce, served on a toasted brioche bun.",
+  13.00
+);
+
+INSERT INTO menu_items (
+  2, "Spicy Chicken Burger","Juicy grilled chicken breast topped with fresh lettuce, ripe tomato, tangy pickles, and a creamy garlic aioli, all served on a toasted brioche bun.",
+  17.00
+);
+
+INSERT INTO menu_items (
+  3, "Classic Cheeseburger", "Juicy beef patty topped with melted cheddar cheese, crisp lettuce, fresh tomato, and tangy pickles, all served on a toasted brioche bun.",
+  15.00
+);
+
+INSERT INTO menu_items (
+  4, "Fries", "Crispy, golden fries seasoned to perfection, served hot and ready to complement any meal.",
+  5.00
+);
+
+INSERT INTO menu_items (
+  5, "Onion Rings", "Crispy, golden-battered onion rings with a satisfying crunch, perfect for dipping or enjoying on their own.",
+  7.00
+);
+
+INSERT INTO menu_items (
+  6, "Coke", "Refreshingly chilled, to quench your thirst.",
+  3.00
+);
+
+INSERT INTO menu_items (
+  7, "Bottle Water", "Refreshingly chilled, to quench your thirst.",
+  3.00
+);
