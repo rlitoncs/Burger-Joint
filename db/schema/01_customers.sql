af496e7d71a354ad6e71e0014ab50252ec8ddb71
Ann <a.m.hnatyshyn@gmail.com>
2024-07-18 12:39:55 -0300

    add customer values to seeds

--- db/schema/01_customers.sql
+++ db/schema/01_customers.sql
@@ -1,8 +1,7 @@
 DROP TABLE IF EXISTS customers CASCADE;
 CREATE TABLE customers (
   id SERIAL PRIMARY KEY NOT NULL,
-  first_name VARCHAR(255) NOT NULL,
-  last_name VARCHAR(255) NOT NULL,
+  name VARCHAR(255) NOT NULL,
   phone_number INTEGER NOT NULL,
   email VARCHAR(255) NOT NULL,
 );
---
+++ db/seeds/01_customers.sql
@@ -0,0 +1,14 @@
+INSERT INTO customers VALUES(
+  "Billie Williams", 7687354762,
+  billiewilliams@gmail.com
+);
+
+INSERT INTO customers VALUES(
+  "Lloyd Finch", 9165414528,
+  lloydfinch@gmail.com
+);
+
+INSERT INTO customers VALUES(
+  "Ebony Bowers", 5492584867,
+  ebonybowers@gmail.com
+);
