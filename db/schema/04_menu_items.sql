046f26cf19a63cdf1a2bde07942f89d17bef63dd
Ann <a.m.hnatyshyn@gmail.com>
2024-07-18 19:34:19 -0300

    edit menu_item column & customers seed file

--- db/schema/04_menu_items.sql
+++ db/schema/04_menu_items.sql
@@ -1,6 +1,7 @@
 DROP TABLE IF EXISTS menu_items CASCADE;
 CREATE TABLE menu_items (
   id SERIAL PRIMARY KEY NOT NULL,
+  image_url IMAGE,
   item_name VARCHAR(80) NOT NULL,
   description VARCHAR(255) NOT NULL,
   price INTEGER NOT NULL,
--- db/seeds/01_customers.sql
+++ db/seeds/01_customers.sql
@@ -1,11 +1,11 @@
 INSERT INTO customers (first_name, last_name, phone, email) VALUES(
-  "Joe", "Smith", 0172222210, joe@joe.ca
+  "Joe", "Smith", 0172222210, joe@gmail.ca
 );
 
 INSERT INTO customers (first_name, last_name, phone, email) VALUES(
-  "Stan", "Jones", 7783493299, stan@stan.ca
+  "Stan", "Jones", 7783493299, stan@gmail.ca
 );
 
 INSERT INTO customers (first_name, last_name, phone, email) VALUES(
-"Lionel", "Brown", 5783493299, leo@leo.ca
+"Lionel", "Brown", 5783493299, leo@gmail.ca
 );
