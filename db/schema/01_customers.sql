929190bd8ed3fe2cb6891d017820068dff0a098d
Ann <a.m.hnatyshyn@gmail.com>
2024-07-17 22:14:36 -0300

    add table files to db

---
+++ db/schema/01_customers.sql
@@ -0,0 +1,8 @@
+DROP TABLE IF EXISTS customers CASCADE;
+CREATE TABLE customers (
+  id SERIAL PRIMARY KEY NOT NULL,
+  first_name VARCHAR(255) NOT NULL,
+  last_name VARCHAR(255) NOT NULL,
+  phone_number INTEGER NOT NULL,
+  email VARCHAR(255) NOT NULL,
+);
---
+++ db/schema/04_menu_items.sql
@@ -0,0 +1,7 @@
+DROP TABLE IF EXISTS menu_items CASCADE;
+CREATE TABLE menu_items (
+  id SERIAL PRIMARY KEY NOT NULL,
+  item_name VARCHAR(80) NOT NULL,
+  description VARCHAR(255) NOT NULL,
+  price INTEGER NOT NULL,
+);
