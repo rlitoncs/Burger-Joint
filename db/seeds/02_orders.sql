7cff472e33446f2e895c4e799a370aea37d7e0ab
rlitoncs <rj_liton@hotmail.com>
2024-07-19 00:56:29 -0400

    Fix: update database scheme and seeds

--- db/seeds/02_orders.sql
+++ db/seeds/02_orders.sql
@@ -1,23 +1,23 @@
 INSERT INTO orders (id, created_at, total_amount, pick_up_time, message,
  order_status, confirmation, customer_id) VALUES(
-  1, 14:30:00, 17.00, 15:00:00, "Your order has been placed",
-  "Order Received", true, 1)
+  1, '2024-07-13 14:30:00', 17.00, '15:00:00', 'Your order has been placed',
+  'Order Received', true, 1
 );
 
 INSERT INTO orders (id, created_at, total_amount, pick_up_time, message,
  order_status, confirmation, customer_id) VALUES(
-  2, 2024-07-15 14:00:00, 17.00, 14:30:00, "Your order has been placed",
-  "Order Received", true, 2)
+  2, '2024-07-15 14:00:00', 17.00, '14:30:00', 'Your order has been placed',
+  'Order Received', true, 2
 );
 
 INSERT INTO orders (id, created_at, total_amount, pick_up_time, message,
  order_status, confirmation, customer_id) VALUES(
-  3, 2024-06-16 12:00:00, 12.00, 12:30:00, "Your order has been placed",
-  "Order Received", true, 3
+  3, '2024-06-16 12:00:00', 12.00, '12:30:00', 'Your order has been placed',
+  'Order Received', true, 3
 );
 
-INSERT INTO orders (id, created_at, total_amount, pick_up_time, message,
- order_status, confirmation, customer_id) VALUES(
-  3, 2024-06-10 13:00:00, 12.00, 13:30:00, "Your order has been placed",
-  "Order Received", true, 3
-);
+-- INSERT INTO orders (id, created_at, total_amount, pick_up_time, message,
+--  order_status, confirmation, customer_id) VALUES(
+--   3, '2024-06-10 13:00:00', 12.00, '13:30:00', 'Your order has been placed',
+--   'Order Received', true, 3
+-- );
--- db/seeds/03__order_items.sql
+++ db/seeds/03__order_items.sql
@@ -1,11 +1,8 @@
-INSERT INTO order_items (id, item_quantity, order_id, menu_item_id) VALUES(
-  1, 1, 1, 3
-);
+INSERT INTO order_items (item_quantity, order_id, menu_item_id) 
+ VALUES(1, 1, 3);
 
-INSERT INTO order_items (id, item_quantity, order_id, menu_item_id) VALUES(
-  2, 1, 2, 5
-);
+INSERT INTO order_items (item_quantity, order_id, menu_item_id)
+  VALUES(1, 2, 5);
 
-INSERT INTO order_items (id, item_quantity, order_id, menu_item_id) VALUES(
-  3, 1, 3, 4
-);
+INSERT INTO order_items (item_quantity, order_id, menu_item_id) 
+  VALUES(1, 3, 4);
