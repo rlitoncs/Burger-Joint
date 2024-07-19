35baa2abd076979f7c5d33983f6a687b1b4db661
rlitoncs <rj_liton@hotmail.com>
2024-07-19 00:23:25 -0400

    Feat: add /menu route to render menu page

--- routes/menu.js
+++ routes/menu.js
@@ -1,15 +1,27 @@
 /*
- * All routes for Users are defined here
- * Since this file is loaded in server.js into /users,
- *   these routes are mounted onto /users
+ * All routes for User Data are defined here
+ * Since this file is loaded in server.js into api/users,
+ *   these routes are mounted onto /api/users
  * See: https://expressjs.com/en/guide/using-middleware.html#middleware.router
  */
 
 const express = require('express');
 const router  = express.Router();
+const userQueries = require('../db/queries/users');
 
 router.get('/', (req, res) => {
-  res.render('menu');
-}); 
+  userQueries.getUsers()
+    .then(burgerItems => {
+      templateVars = {
+        burgers: burgerItems
+      }
+      res.render('menu', templateVars); //array of objects
+    })
+    .catch(err => {
+      res
+        .status(500)
+        .json({ error: err.message });
+    });
+});
 
 module.exports = router;
