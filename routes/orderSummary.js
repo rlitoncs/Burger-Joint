d19654ee02fa9266d78a1f8dae43deb9902bb960
rlitoncs <rj_liton@hotmail.com>
2024-07-21 21:37:23 -0400

    Feat: update routing for order submission, and post data to database

--- routes/orderSummary.js
+++ routes/orderSummary.js
@@ -7,9 +7,11 @@
 
 const express = require('express');
 const router  = express.Router();
+const orderQueries = require('../db/queries/orders');
 
 const orderCart = []; //use as tempalte vars
 
+// GET /orderSummary
 router.get('/', (req, res) => {
   templateVars = {
     orderSummary: orderCart
@@ -17,6 +19,7 @@ router.get('/', (req, res) => {
   res.render('orderSummary', templateVars);
 });
 
+// POST /orderSummary
 router.post('/', (req, res) => {
   const menuItem = req.body;
   if(orderCart.length === 0){
@@ -32,5 +35,24 @@ router.post('/', (req, res) => {
  
   res.redirect("/menu");
 });
+
+
+// POST /orderSummary/order-submitted
+router.post('/order-submitted', (req, res) => {
+  const customerID = 3; // we would get the id from the cookie session
+
+  for (let item of orderCart) {
+    orderQueries.addOrder('now()', Number(item.price) * Number(item.quantity), '12:30:00', 'Your order has been placed', 'Order Received', true, customerID);
+
+    orderQueries.getSpecificOrder(customerID)
+    .then(orderID => {
+      console.log('order ID from getSpecificOrder()', orderID.reverse()[0], orderID.reverse()[0].id)
+      orderQueries.addOrderItems(Number(item.quantity), orderID.reverse()[0].id, Number(item.id));
+    });
+  }
+
+  res.render('orderComplete');
+});
+
 
 module.exports = router;
