c65cce342425fe8f25aa00c6789b607679c1d660
rlitoncs <rj_liton@hotmail.com>
2024-07-24 21:13:42 -0400

    Fix: update notification message on restaurant side

--- routes/chefOrders.js
+++ routes/chefOrders.js
@@ -21,26 +21,9 @@ router.get('/', (req, res) => {
 
 // POST orders
 router.post('/:id', (req, res) => {
-  const pickUpTime = req.body.pick_up_time;
+  const pickUpTimeMsg = req.body.pick_up_time;
 
-  const customerID = Number(req.body.customer_id);
-  const customerName = req.body.customer_first_name + ' ' + req.body.customer_last_name;
-
-  const orderID = Number(req.params.id); //used update orders table
-  orderQueries.setPickUpTime(orderID, customerID, customerName, pickUpTime)
-
-  //Formatting Time for SMS message (24 hr format -> 12 hr format)
-  let formatPickUpTime = pickUpTime.slice(0,5); //e.g. 10:00:00
-  let formatPickUpTimeHour = formatPickUpTime.slice(0,2);
-  if(Number(formatPickUpTimeHour) > 12) {
-    let convertToTwelve = Number(formatPickUpTimeHour.slice(1,2)) - 2 ;
-    formatPickUpTime = String(convertToTwelve) + formatPickUpTime.slice(2,5) + ' PM';
-
-  } else {
-    formatPickUpTime += ' AM';
-  };
-
-  sendSMS(`Hello ${customerName}. Your order(#${orderID}) will be ready for pick up at ${formatPickUpTime}! `)
+  sendSMS(`${pickUpTimeMsg}`);
 
   res.redirect('/chefOrders'); //template vars
 
