e7db0ef3818f34b8a8bea079359985776a4543b3
rlitoncs <rj_liton@hotmail.com>
2024-07-25 03:06:23 -0400

    Fix: update values to be inserted

--- db/seeds/02_orders.sql
+++ db/seeds/02_orders.sql
@@ -1,17 +1,17 @@
 INSERT INTO orders (created_at, total_amount, pick_up_time, message,
  order_status, confirmation, customer_id) VALUES(
-  '2024-07-13 14:30:00', 17.00, '15:00:00', 'Your order has been placed',
+  '2024-07-13 14:30:00', 13.00, '15:00:00', 'Your order has been placed',
   'Order Received', true, 1
 );
 
 INSERT INTO orders (created_at, total_amount, pick_up_time, message,
  order_status, confirmation, customer_id) VALUES(
-  '2024-07-15 14:00:00', 17.00, '14:30:00', 'Your order has been placed',
+  '2024-07-15 14:00:00', 7.00, '14:30:00', 'Your order has been placed',
   'Order Received', true, 2
 );
 
 INSERT INTO orders (created_at, total_amount, pick_up_time, message,
  order_status, confirmation, customer_id) VALUES(
-  '2024-06-16 12:00:00', 12.00, '12:30:00', 'Your order has been placed',
+  '2024-06-16 12:00:00', 5.00, '12:30:00', 'Your order has been placed',
   'Order Received', true, 3
 );
