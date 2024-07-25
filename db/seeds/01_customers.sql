e3a8d1140274c029432870f7251db39b9349e5f6
Henry <mbahenryemeka@gmail.com>
2024-07-24 20:00:12 -0400

    final update

--- db/seeds/01_customers.sql
+++ db/seeds/01_customers.sql
@@ -1,15 +1,4 @@
 INSERT INTO customers (first_name, last_name, phone_number, email) VALUES(
-<<<<<<< HEAD
-  'Joe', 'Smith', 0172222210, 'joe@gmail.ca'
-);
-
-INSERT INTO customers (first_name, last_name, phone_number, email) VALUES(
-  'Stan', 'Jones', 7783493299, 'stan@gmail.ca'
-);
-
-INSERT INTO customers (first_name, last_name, phone_number, email) VALUES(
-'Lionel', 'Brown', 5783493299, 'leo@gmail.ca'
-=======
   'Joe', 'Smith', '0172222210', 'joe@gmail.ca' 
 );
 
@@ -19,5 +8,4 @@ INSERT INTO customers (first_name, last_name, phone_number, email) VALUES(
 
 INSERT INTO customers (first_name, last_name, phone_number, email) VALUES(
 'Lionel', 'Brown', '5783493299', 'leo@gmail.ca'
->>>>>>> master
 );
--- db/seeds/02_orders.sql
+++ db/seeds/02_orders.sql
@@ -1,28 +1,3 @@
-<<<<<<< HEAD
-INSERT INTO orders (id, created_at, total_price, pick_up_time, message,
- order_status, confirmation, customers_id) VALUES(
-  1, '2024-07-15 14:30:00', 17.00, '2024-07-15 15:00:00', 'Your order has been placed',
-   true, true, 1);
-
-
-INSERT INTO orders (id, created_at, total_price, pick_up_time, message,
- order_status, confirmation, customers_id) VALUES(
-  2, '2024-07-15 14:00:00', 17.00, '2024-07-15 14:30:00', 'Your order has been placed',
-   true, true, 2);
-
-
-INSERT INTO orders (id, created_at, total_price, pick_up_time, message,
- order_status, confirmation, customers_id) VALUES(
-  3, '2024-06-16 12:00:00', 12.00, '2024-06-16 12:30:00', 'Your order has been placed',
-   true, true, 3
-);
-
-INSERT INTO orders (id, created_at, total_price, pick_up_time, message,
- order_status, confirmation, customers_id) VALUES(
-  4, '2024-06-10 13:00:00', 12.00, '2024-06-10 13:30:00', 'Your order has been placed',
-   true, true, 3
-);
-=======
 INSERT INTO orders (created_at, total_amount, pick_up_time, message,
  order_status, confirmation, customer_id) VALUES(
   '2024-07-13 14:30:00', 17.00, '15:00:00', 'Your order has been placed',
@@ -40,4 +15,3 @@ INSERT INTO orders (created_at, total_amount, pick_up_time, message,
   '2024-06-16 12:00:00', 12.00, '12:30:00', 'Your order has been placed',
   'Order Received', true, 3
 );
->>>>>>> master
